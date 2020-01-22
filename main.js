b0ad856a2d2abca933bd2ba106c6db07d9783882
SashaPlavi <sasha.bingulac@gmail.com>
2020-01-22 19:16:44 +0100

    showing alerts

--- main.js
+++ main.js
@@ -33,6 +33,24 @@ class UI {
     `;
     list.appendChild(row);
   }
+  static deleteBook(el) {
+    if (el.classList.contains("delete")) {
+      console.log(el.classList);
+
+      el.parentElement.parentElement.remove();
+    }
+  }
+  static showAlert(msg, className) {
+    const div = document.createElement("div");
+    div.className = `alert alert-${className}`;
+    div.appendChild(document.createTextNode(msg));
+    const container = document.querySelector(".container");
+    const form = document.querySelector("#book-form");
+    container.insertBefore(div, form);
+    setTimeout(() => {
+      document.querySelector(".alert").remove();
+    }, 2000);
+  }
   static clearFilds() {
     document.getElementById("title").value = "";
     document.getElementById("author").value = "";
@@ -52,10 +70,19 @@ document.getElementById("book-form").addEventListener("submit", e => {
   const title = document.getElementById("title").value;
   const author = document.getElementById("author").value;
   const isbn = document.getElementById("isbn").value;
+  if (title === "" || author === "" || isbn === "") {
+    UI.showAlert("Please fill in all fields", "danger");
+  }
   const book = new Book(title, author, isbn);
   console.log(book);
   UI.addBookToList(book);
+  UI.showAlert("Book Added", "success");
   UI.clearFilds();
 
   // return book;
+});
+
+document.getElementById("book-list").addEventListener("click", e => {
+  UI.deleteBook(e.target);
+  UI.showAlert("Book Deleted", "success");
 });
